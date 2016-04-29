cdebd44b7a8e875611dee8f6acf598c7e00b0d06
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-04-29 12:48:06 -0400

    made simian unit test less specific

--- test/testGit.rb
+++ test/testGit.rb
@@ -37,8 +37,8 @@ class GitTest < Test::Unit::TestCase
   def test_that_simian_returns_valid_response
     url = 'https://github.com/michaelrauh/RomanNumeralConverter'
     git_clone(url)
-    actual = run_simian(git_extract(url))
-    expected = "Similarity Analyser 2.4.0 - http://www.harukizaemon.com/simian\nCopyright (c) 2003-2015 Simon Harris.  All rights reserved.\nSimian is not free unless used solely for non-commercial or evaluation purposes.\n{failOnDuplication=true, ignoreCharacterCase=true, ignoreCurlyBraces=true, ignoreIdentifierCase=true, ignoreModifiers=true, ignoreStringCase=true, threshold=2}\nFound 0 duplicate lines in 0 blocks in 0 files\nProcessed a total of 0 significant (0 raw) lines in 0 files\nProcessing time: 0.005sec\n"
-    assert_equal expected, actual
+    actual = run_simian("#{git_extract(url)}/*")
+
+    assert actual.include?("Processing time")
   end
 end
