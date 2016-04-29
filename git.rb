51323f32aa79c77d5ee5aa887c5b5102713d4b8f
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-04-29 12:36:18 -0400

    added call to Simian in backend to check for duplicated code

--- git.rb
+++ git.rb
@@ -18,3 +18,7 @@ end
 def run_tests(command)
   %x(#{command} 2>&1)
 end
+
+def run_simian(files)
+  %x(java -jar simian-2.4.0.jar -threshold=2 #{files} 2>&1)
+end
--- test/testGit.rb
+++ test/testGit.rb
@@ -20,6 +20,7 @@ class GitTest < Test::Unit::TestCase
     git_cd(git_extract(url))
     commits = git_count_commits
     assert_equal commits, 69
+    git_cd("../")
     %x`rm -rf RomanNumeralConverter`
   end
 
@@ -28,8 +29,16 @@ class GitTest < Test::Unit::TestCase
     git_clone(url)
     git_cd(git_extract(url))
     test_response = run_tests("python number_converter_test.py")
+    git_cd("../")
     expected_response = ".......................\n----------------------------------------------------------------------\nRan 23 tests in 0.001s\n\nOK\n"
     assert_equal expected_response, test_response
   end
 
+  def test_that_simian_returns_valid_response
+    url = 'https://github.com/michaelrauh/RomanNumeralConverter'
+    git_clone(url)
+    actual = run_simian(git_extract(url))
+    expected = "Similarity Analyser 2.4.0 - http://www.harukizaemon.com/simian\nCopyright (c) 2003-2015 Simon Harris.  All rights reserved.\nSimian is not free unless used solely for non-commercial or evaluation purposes.\n{failOnDuplication=true, ignoreCharacterCase=true, ignoreCurlyBraces=true, ignoreIdentifierCase=true, ignoreModifiers=true, ignoreStringCase=true, threshold=2}\nFound 0 duplicate lines in 0 blocks in 0 files\nProcessed a total of 0 significant (0 raw) lines in 0 files\nProcessing time: 0.005sec\n"
+    assert_equal expected, actual
+  end
 end
