c4fb675b54d803c7873e977532e23e167432fd6f
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-05-02 13:11:40 -0400

    file extension is being passed from the user. Only option right now is python

--- main.rb
+++ main.rb
@@ -12,13 +12,14 @@ post '/confirm/' do
   from = params[:from] || "Nowhere"
   test_command = params[:testCommand] || "None"
   to = params[:to] || "mrauh+default@pillartechnology.com"
+  extension = params[:extension] || '.rb'
 
   project_name = git_extract(kataURL)
   git_clone(kataURL)
   git_cd(project_name)
   commit_number = git_count_commits
   test_results = run_tests(test_command)
-  number_of_tests = count_all('.py')
+  number_of_tests = count_all(extension)
   git_cd('../')
   simian_results = run_simian("#{project_name}/*")
 
