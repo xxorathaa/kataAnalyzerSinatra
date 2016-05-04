41acb6e809f40ea0dfac84745e1869ee74b8cabb
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-05-04 16:51:23 -0400

    added tests for javacript complexity

--- test/testComplexity.rb
+++ test/testComplexity.rb
@@ -6,29 +6,36 @@ class ComplexityTest < Test::Unit::TestCase
 def test_that_ruby_complexity_can_be_counted
   Dir.chdir("../testData/ruby/production")
   total = count_all('.rb')
-  assert_equal 46, total
   Dir.chdir('../../')
+  assert_equal 46, total
 end
 
 def test_that_cpp_complexity_can_be_counted
   Dir.chdir('../testData/cpp/production')
   total = count_all('.cpp') + count_all('.cc') + count_all('.c++')
-  assert_equal 11, total
   Dir.chdir('../../')
+  assert_equal 11, total
 end
 
 def test_that_csharp_complexity_can_be_counted
   Dir.chdir('../testData/csharp/production')
   total = count_all('.cs')
-  assert_equal 6, total
   Dir.chdir('../../')
+  assert_equal 6, total
 end
 
 def test_that_java_complexity_can_be_counted
   Dir.chdir('../testData/java/production')
   total = count_all('.java')
-  assert_equal 15, total
   Dir.chdir('../../')
+  assert_equal 15, total
+end
+
+def test_that_javascript_complexity_can_be_counted
+  Dir.chdir('../testData/javascript/production')
+  total = count_all('.js')
+  Dir.chdir('../../')
+  assert_equal 9, total
 end
 
 end
