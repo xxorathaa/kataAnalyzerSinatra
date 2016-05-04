08470cc75bb6b6ceb64e9bfe78e87bc03a9b1ba4
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-05-04 16:16:40 -0400

    Added and equal for ruby complexity

--- complexity.rb
+++ complexity.rb
@@ -1,6 +1,6 @@
 def count(file_contents)
   complexity = 1
-  patterns = [/\Wif\W/, /\Wunless\W/, /\Wwhile\W/, /\Wuntil\W/, /\Wfor\W/, /\Welsif\W/, /\Wwhen\W/, /\Wrescue\W/, /\&\&/, /\Wand\W/, /\|\|/, /\Wor\W/]
+  patterns = [/\Wif\W/, /\Wunless\W/, /\Wwhile\W/, /\Wuntil\W/, /\Wfor\W/, /\Welsif\W/, /\Wwhen\W/, /\Wrescue\W/, /\&\&/, /\Wand\W/, /\|\|/, /\Wor\W/, /&=/]
     patterns.each do |pattern|
     complexity += file_contents.scan(pattern).count
   end
