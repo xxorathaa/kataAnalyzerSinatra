4b5b6f706756428a3b7a1d673ed71b9fee6f0d90
Michael Rauh <michaelrauh@MichaelRauh.local>
2016-05-04 16:28:35 -0400

    Added cpp testing and found some missed in the ruby code

--- test/testComplexity.rb
+++ test/testComplexity.rb
@@ -6,6 +6,14 @@ class ComplexityTest < Test::Unit::TestCase
 def test_that_ruby_complexity_can_be_counted
   Dir.chdir("../testData/ruby/production")
   total = count_all('.rb')
-  assert_equal 43, total
+  assert_equal 46, total
+  Dir.chdir('../../../')
+end
+
+def test_that_cpp_complexity_can_be_counted
+  Dir.chdir('../testData/cpp/production')
+  total = count_all('.cpp') + count_all('.cc') + count_all('.c++')
+  assert_equal 11, total
+  Dir.chdir('../../')
 end
 end
